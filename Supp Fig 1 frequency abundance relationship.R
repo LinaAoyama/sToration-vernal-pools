57d9173409d44c958e83def8c1eb565cf2bc0905
LinaAoyama <44237633+LinaAoyama@users.noreply.github.com>
2021-07-23 11:08:06 -0700

    Update Supp Fig 1 frequency abundance relationship.R

--- Supp Fig 1 frequency abundance relationship.R
+++ Supp Fig 1 frequency abundance relationship.R
@@ -1,16 +1,15 @@
 # Exploring the relationship between frequency and abundance
 # This will allow reference data (frequency) to be transformed to abundance
 # The abundance estimates will be used to parameterize a Beverton-Holt population model
-# This addresses Q3 and Q4 in the sToration group
 
+# Packages
 library(tidyverse)
 library(gridExtra)
 library(extrafont)
 
-### Source the data
-source("data_compiling/compile_composition.R")
-source("data_compiling/compile_constructed_depth.R")
-source("data_compiling/compile_reference_depth.R")
+# Data
+source("compile_composition.R")
+
 
 ### Get model for LACO
 # Isolate LACO as species of interest
